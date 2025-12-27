8349a6a6850bd05ae8ab57155d55736846fe9e87
Zakia Al Kadri <alkadrizakia14@gmail.com>
2025-12-27 12:55:11 -0600

    Enhance documentation on data representations and pickle
    Added explanation about data representations and the purpose of pickle.

--- pickle_example.py
+++ pickle_example.py
@@ -1,6 +1,10 @@
 '''
+Programs work with data in different representations, depending on whether the data is:
+1) In memory (inside a running program): Data is stored as objects, structs, lists, arrays, hash tables, trees, etc.
+2) Stored in a file or sent over a network: We must encode the data as a Self-contained sequence of bytes that can be understood independently of memory addresses
+
 # What is Pickle?
-`pickle` is used for **serializing and deserializing** Python objects.
+`pickle` is used for serializing and deserializing Python objects.
 It converts in-memory objects into a byte stream that can be stored or transmitted.
 
 # Key Concepts
